227a716959f4bedbef4ad58b673c26808753c0ce
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-02-14 16:12:52 -0600

    imported Route and Routes from react-router-dom; imported nav pages in App; created different routes that link to different pages

--- capstone-project/src/App.jsx
+++ capstone-project/src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { MakeAPrayerRequest } from './pages/MakeAPrayerRequest';
+import { Signin } from './pages/Signin';
+import { Signup } from './pages/Signup';
 
 function App() {
     return(
-    <>
-    </>)
+    <Routes>
+        <Route index element={<Home />} />
+        <Route path='/prayerrequest' element={<MakeAPrayerRequest />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='*' element={<h1>404: Page Not Found</h1>} />
+    </Routes>)
 }
 
 export default App;
