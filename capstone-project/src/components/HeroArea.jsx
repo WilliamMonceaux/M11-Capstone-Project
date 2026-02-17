41d02b6a4911daad800dfeb86154a1a355388fe4
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-02-17 07:31:01 -0600

    imported Typography; included Heading and Paragraph children for HeroArea reusability; added some semanticness to the structure; changed hero area properties to make wrapper for header and paragraph text more responsive; added Header container with sample text

--- capstone-project/src/components/HeroArea.jsx
+++ capstone-project/src/components/HeroArea.jsx
@@ -1,26 +1,35 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
-function HeroArea() {
+function HeroArea({ Heading, Paragraph}) {
   return (
     <Box
+    component="section"
       sx={{
         display: 'flex',
         alignItems: 'center',
         flexDirection: 'column',
         minHeight: '60vh',
         background: 'linear-gradient(to bottom, #43E97B, #38F9D7)',
-        width: '100%'
+        width: '100%',
       }}
     >
       <Box
         sx={{
           display: 'flex',
-          flex: 'stretch',
-          backgroundColor: '#f5f5f5',
-          p: '10rem',
+          flexDirection: 'row',
+          alignItems: 'start',
+          justifyContent: 'center',
+          backgroundColor: 'white',
           width: '70%',
+          flexGrow: 1,
         }}
       >
+        <Box sx={{ width: '100%', m: '4rem', textAlign: 'center' }}>
+          <Typography variant="h1" component="h1" sx={{ fontSize: '8.5rem' }}>
+            Where healing and community is built
+          </Typography>
+        </Box>
+        
       </Box>
     </Box>
   );
