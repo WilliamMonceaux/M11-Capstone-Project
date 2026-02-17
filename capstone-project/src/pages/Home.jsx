c3822060b6f19952a1119677d19149a7c79b7440
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-02-17 09:25:13 -0600

    imported UnderstandingPrayer component and rendering it

--- capstone-project/src/pages/Home.jsx
+++ capstone-project/src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import '../index.css';
 import { HeroArea } from '../components/HeroArea';
+import { UnderstandingPrayer } from '../components/UnderstandingPrayer';
 
 function Home() {
   return (
+    <>
     <HeroArea
       heading="Where healing and community is built"
       paragraph="Join a community dedicated to lifting one another up. Share your prayer request and experience the strength of being known and supported"
       button="Create an Account"
     />
+    <UnderstandingPrayer />
+    </>
   );
 }
 
