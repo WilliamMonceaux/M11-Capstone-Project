23acb698ad2a80d31d42d4d1a89026f100f7341d
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-02-17 17:06:41 -0600

    created new containers that holds the prayer status and the time its been since request was uploaded; added styling and layout to these containers

--- capstone-project/src/components/PrayerRequestCards.jsx
+++ capstone-project/src/components/PrayerRequestCards.jsx
@@ -50,7 +50,7 @@ function PrayerRequestCards() {
               <Avatar alt="User Profile" sx={{ fontSize: '1.4rem', color: 'black' }} />
               <Typography
                 variant="h6"
-                sx={{ fontSize: { xs: '1.5rem', md: '2.0rem' } }}
+                sx={{ fontSize: { xs: '1.5rem', md: '2.0rem'} }}
               >
                 Username
               </Typography>
@@ -62,6 +62,23 @@ function PrayerRequestCards() {
           <Typography variant='body1' sx={{ mt: 1, mb: 4, fontSize: { xs: '1.2rem', md: '1.6rem' }}}>
             This is a paragraph.
           </Typography>
+          <Box sx={{ display: 'flex', alignItems: 'center', border: '2px solid pink', mt: 'auto' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', flexGrow: 1, gap: 1, border: '2px solid lime' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, border: '2px solid yellow' }}>
+                <Typography variant='h6' sx={{ fontSize: { xs: '1.5rem', md: '2rem' } }}>
+                    Status:
+                </Typography>
+                <Typography variant='body1' sx={{ fontSize: { xs: '1.2rem', md: '1.6rem' }, fontWeight: 'bold'}}>
+                    Need Prayers
+                </Typography>
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', border: '2px solid green'}}>
+                <Typography sx={{ fontSize: { xs: '1.2rem', md: '1.6rem' }, fontWeight: 'bold'}}>
+                    20 minutes ago
+                </Typography>
+                </Box>
+            </Box>
+          </Box>
         </Box>
       </Container>
     </Box>
