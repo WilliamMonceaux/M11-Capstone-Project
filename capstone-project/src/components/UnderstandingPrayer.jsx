26ce5f74542203317ca3e922ff64cd35838ce1eb
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-02-17 11:43:30 -0600

    imported Box, Container, Grid, Typography from MUI material; created understanding prayer parent container along with its children containers to hold prayer information

--- capstone-project/src/components/UnderstandingPrayer.jsx
+++ capstone-project/src/components/UnderstandingPrayer.jsx
@@ -1,8 +1,21 @@
+import { Box, Container, Grid, Typography } from '@mui/material';
 
 function UnderstandingPrayer() {
-    return(
-    <>
-    </>)
+  return (
+    <Box sx={{ minHeight: '60vh', display: 'flex', justifyContent: 'center', p: 0, my: 10, border: '2px solid brown' }}>
+    <Grid
+      container
+      component="section"
+      direction='column'
+      sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, width: '100%', p: 0, border: '2px solid red' }}
+    >
+    </Grid>
+      <Container
+        maxWidth="lg"
+        sx={{ flexGrow: 1, p: 15, border: '2px solid green', width: '100%' }}
+      ></Container>
+    </Box>
+  );
 }
 
-export { UnderstandingPrayer }; 
+export { UnderstandingPrayer };
