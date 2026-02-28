71e14da3962a9c519dd775d5fd29c03f0f466f93
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-02-28 15:17:21 -0600

    imported User model; fixed a naming mismatch of 'Post' into 'prayer_id' that was causing GET errors; changed author into user_id to fix naming mismatch; changed missing field text

--- prayer-request-website/src/app/api/comments/[id]/route.js
+++ prayer-request-website/src/app/api/comments/[id]/route.js
@@ -1,13 +1,14 @@
 import { connectMongo } from '@/lib/mongodb';
 import { Comment } from '@/models/Comment';
+import { User } from '@/models/User';
 import { NextResponse } from 'next/server';
 
 export async function GET(req, { params }) {
   try {
     await connectMongo();
     const { id } = await params;
-    const comments = await Comment.find({ post: id })
-      .populate('author', 'username profilePicture')
+    const comments = await Comment.find({ prayer_id: id })
+      .populate('user_id', 'username profilePicture')
       .sort({ createdAt: -1 });
 
     return NextResponse.json(comments, { status: 200 });
@@ -25,23 +26,23 @@ export async function POST(req, { params }) {
 
     const { id } = await params;
 
-    const { content, author } = await req.json();
+    const { content, user_id } = await req.json();
 
-    if (!content || !author) {
+    if (!content || !user_id) {
       return NextResponse.json(
-        { error: 'Comment content and author are required' },
+        { error: 'Missing content or user_id' },
         { status: 400 }
       );
     }
 
     const newComment = await Comment.create({
       content,
-      author,
-      post: id,
+      user_id,
+      prayer_id: id,
     });
 
     const populatedComment = await newComment.populate(
-      'author',
+      'user_id',
       'username profilePicture'
     );
 
