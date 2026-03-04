dd156be4714c35d13617f7f5b6e4a25068a1ec21
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-03-04 17:29:22 -0600

    imported Arrow image and Link; changed font weight to a lighter more ledgable text; added styling to Arrow image with button

--- prayer-request-website/src/components/RequestBtn.jsx
+++ prayer-request-website/src/components/RequestBtn.jsx
@@ -1,4 +1,6 @@
 import { Button, Grid } from '@mui/material';
+import Arrow from 'next/image';
+import Link from 'next/link';
 
 function RequestBtn({ text }) {
   return (
@@ -6,15 +8,31 @@ function RequestBtn({ text }) {
       {text && (
         <Grid item sx={{ my: 4 }}>
           <Button
+          component={Link}
+          href='/request'
             variant="contained"
             sx={{
               fontSize: { xs: '1.1rem', md: '1.4rem' },
-              fontWeight: 700,
+              fontWeight: 500,
               p: '1.2rem',
               backgroundColor: '#2196F3',
+              width: { xs: '200px', md: '300px' },
+              borderRadius: 6,
+              textTransform: 'none',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              gap: 4,
             }}
           >
             {text}
+            <Arrow
+              src="/images/arrow.png"
+              alt="Arrow"
+              width={32}
+              height={24}
+              style={{ filter: 'brightness(0) invert(1)' }}
+            />
           </Button>
         </Grid>
       )}
