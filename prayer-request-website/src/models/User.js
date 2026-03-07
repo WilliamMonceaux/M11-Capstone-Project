2e8326e0b3946d039f7807d61adb4f5ef3a141e4
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-03-07 15:02:57 -0600

    moved password hashing to mongoose user model; changed import for mongoose

--- prayer-request-website/src/models/User.js
+++ prayer-request-website/src/models/User.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs'
 
 const userSchema = new mongoose.Schema(
   {
@@ -34,7 +35,19 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 module.exports = {
   User,
