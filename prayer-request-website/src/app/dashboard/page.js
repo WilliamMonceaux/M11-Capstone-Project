6870e347b0deed733eedb42c6428c255c6ea7017
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-03-11 19:30:39 -0500

    refactor(dashboard): implement dynamic client-side entry point since there was a client-side error

--- prayer-request-website/src/app/dashboard/page.js
+++ prayer-request-website/src/app/dashboard/page.js
@@ -1,65 +1,11 @@
 "use client";
-import CssBaseline from '@mui/material/CssBaseline';
-import { createHashRouter, RouterProvider } from 'react-router';
-import DashboardLayout from '@/components/DashboardLayout';
-import EmployeeList from '@/components/EmployeeList';
-import EmployeeShow from '@/components/EmployeeShow';
-import EmployeeCreate from '@/components/EmployeeCreate';
-import EmployeeEdit from '@/components/EmployeeEdit';
-import NotificationsProvider from '@/hooks/useNotifications/NotificationsProvider';
-import DialogsProvider from '@/hooks/useDialogs/DialogsProvider';
-import AppTheme from '@/theme/customizations/AppTheme'; 
-import {
-  dataGridCustomizations,
-  datePickersCustomizations,
-  sidebarCustomizations,
-  formInputCustomizations,
-} from '@/theme/customizations';
 
-const router = createHashRouter([
-  {
-    Component: DashboardLayout,
-    children: [
-      {
-        path: '/employees',
-        Component: EmployeeList,
-      },
-      {
-        path: '/employees/:employeeId',
-        Component: EmployeeShow,
-      },
-      {
-        path: '/employees/new',
-        Component: EmployeeCreate,
-      },
-      {
-        path: '/employees/:employeeId/edit',
-        Component: EmployeeEdit,
-      },
-      {
-        path: '*',
-        Component: EmployeeList,
-      },
-    ],
-  },
-]);
+import dynamic from 'next/dynamic';
 
-const themeComponents = {
-  ...dataGridCustomizations,
-  ...datePickersCustomizations,
-  ...sidebarCustomizations,
-  ...formInputCustomizations,
-};
+const DashboardClient = dynamic(() => import('@/components/DashboardClient'), {
+  ssr: false,
+});
 
-export default function CrudDashboard(props) {
-  return (
-    <AppTheme {...props} themeComponents={themeComponents}>
-      <CssBaseline enableColorScheme />
-      <NotificationsProvider>
-        <DialogsProvider>
-          <RouterProvider router={router} />
-        </DialogsProvider>
-      </NotificationsProvider>
-    </AppTheme>
-  );
+export default function Page() {
+  return <DashboardClient />;
 }
