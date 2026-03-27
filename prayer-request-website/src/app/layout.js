fb66cf703d4999a4db012639fe179b0dd4e958c9
WilliamMonceaux <williammonceauxalt@gmail.com>
2026-03-27 08:22:46 -0500

    refactor: update theme import path and resolve theme config error

--- prayer-request-website/src/app/layout.js
+++ prayer-request-website/src/app/layout.js
@@ -1,7 +1,7 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from '@/lib/theme';
+import theme from '@/lib/index.js';
 import { Roboto, Lato } from 'next/font/google';
 import './globals.css';
 import { Navbar } from '@/components/Navbar';
